817fdeeccb28c7c9d8d1638adae2278f01c77e63
kaiap16 <kaiapiccinino@gmail.com>
2023-12-18 21:39:22 +0100

    experiments with sidebar positions

--- app/public/wp-content/themes/icatheme/front-page.php
+++ app/public/wp-content/themes/icatheme/front-page.php
@@ -3,33 +3,11 @@
 <!-- The Loop -->
 
 	<h1>Index</h1>
-    
-		<div class="row">
 
-			<div class="col-4">
-			<?php get_sidebar(); ?>
-
-			</div>	
-				<div class="col-6">
-			<?php if(have_posts()) : while (have_posts()) : the_post() ?>
 
 				<?php 
 					the_content(); 
-					endwhile;
-					endif;
+				
 				?>
-					</div>
-		</div>	
-
-
-	
-	
-		<!-- <div class="row"> -->
-<!-- The Loop -->
-<!-- <?php 
-	$args = array('section_title' => 'Home');
-	get_template_part("template-parts/loop", null, $args);
-?> -->
-		
-	</div>
+					
 <?php get_footer(); ?>
